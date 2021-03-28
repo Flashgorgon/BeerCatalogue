54bea2b97221cc93f88d9b1a4caa026ca17a7bf4
isola <isola@opentrends.net>
2021-03-28 22:33:01 +0200

    Rename test package

--- src/test/java/com/haufe/beerCatalogue/BeerCatalogueApplicationTests.java
+++ src/test/java/com/haufe/beerCatalogue/BeerCatalogueApplicationTests.java
@@ -1,4 +1,4 @@
-package com.haufe.beer;
+package com.haufe.beerCatalogue;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
